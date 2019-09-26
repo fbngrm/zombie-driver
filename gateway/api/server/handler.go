ce33cbd97cd7054aff12cfe4173b082a7ab13c74
fbngrm <fbngrm@gmail.com>
2019-09-26 10:06:03 +0200

    move handler code to own module

--- gateway/api/server/handler.go
+++ gateway/api/server/handler.go
@@ -1,1 +1,55 @@
 package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// TODO: CORS
+func newHandler(u URL) (http.Handler, error) {
+	p, err := u.protocol()
+	if err != nil {
+		return nil, err
+	}
+	switch p {
+	case NSQ:
+		return &nsqHandler{}, nil
+	case HTTP:
+		return &httpHandler{}, nil
+	default:
+		return nil, fmt.Errorf("no handler found for %s", p)
+	}
+}
+
+type location struct {
+	Lat  float64 `json:"latitude"`
+	Long float64 `json:"longitude"`
+}
+
+type nsqHandler struct{}
+
+func (n *nsqHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		// FIXME
+		panic(err)
+	}
+	var l location
+	err = json.Unmarshal(body, &l)
+	if err != nil {
+		// FIXME
+		panic(err)
+	}
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Hi %s, I am at %+v!", vars["id"], l)
+}
+
+type httpHandler struct{}
+
+func (h *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
--- gateway/api/server/server.go
+++ gateway/api/server/server.go
@@ -2,9 +2,7 @@ package server
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -35,22 +33,6 @@ func New(cfg *config) (*Server, error) {
 	}, nil
 }
 
-// TODO: CORS
-func newHandler(u URL) (http.Handler, error) {
-	p, err := u.protocol()
-	if err != nil {
-		return nil, err
-	}
-	switch p {
-	case NSQ:
-		return &nsqHandler{}, nil
-	case HTTP:
-		return &httpHandler{}, nil
-	default:
-		return nil, fmt.Errorf("no handler found for %s", p)
-	}
-}
-
 func (s *Server) Run(ctx context.Context, addr string) error {
 	s.server.Handler = s.Mux
 
@@ -70,33 +52,4 @@ func (s *Server) Run(ctx context.Context, addr string) error {
 	<-ctx.Done()
 
 	return err
-}
-
-type location struct {
-	Lat  float64 `json:"latitude"`
-	Long float64 `json:"longitude"`
-}
-
-type nsqHandler struct{}
-
-func (n *nsqHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		// FIXME
-		panic(err)
-	}
-	var l location
-	err = json.Unmarshal(body, &l)
-	if err != nil {
-		// FIXME
-		panic(err)
-	}
-	vars := mux.Vars(r)
-	fmt.Fprintf(w, "Hi %s, I am at %+v!", vars["id"], l)
-}
-
-type httpHandler struct{}
-
-func (h *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
