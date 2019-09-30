bc63ed970e0c6d12230153c2a42ac8a273cdd4d2
fbngrm <fbngrm@gmail.com>
2019-09-30 16:01:35 +0200

    add minimal logger setup

--- gateway/cmd/gateway/cli/cli.go
+++ gateway/cmd/gateway/cli/cli.go
@@ -2,12 +2,27 @@ package cli
 
 import (
 	"context"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/heetch/FabianG-technical-test/gateway/api/server"
+	"github.com/rs/zerolog"
 )
+
+// Using default log level debug and write to stderr.
+// Note: We log in (inefficient) human friendly format to console here since it
+// is a coding challenge. In a production environment we would prefer structured,
+// machine parsable format. So we could make use of automated log analysis e.g.
+// error reporting.
+func NewLogger() zerolog.Logger {
+	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	logger = logger.Output(zerolog.ConsoleWriter{Out: f})
+	log.SetFlags(0)
+	log.SetOutput(logger)
+	return logger
+}
 
 func RunServer(srv *server.Server, addr string) error {
 	ctx, cancel := context.WithCancel(context.Background())
