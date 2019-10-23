9590d95e1b8faa8ef5799c1848ec29f67fff6545
fbngrm <fbngrm@gmail.com>
2019-10-23 11:18:43 +0200

    move Error type to http

--- handler/http.go
+++ handler/http.go
@@ -3,13 +3,26 @@ package handler
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/hlog"
 )
 
-// Copied from https://github.com/heetch/regula/blob/master/api/server/handler.go
+// Copied from https://github.com/heetch/regula/blob/master/api/types.go
+type Error struct {
+	Err      string         `json:"error"`
+	Response *http.Response `json:"-"` // Will not be marshalled
+}
+
+func (e Error) Error() string {
+	return fmt.Sprintf("%v %v: %d %v",
+		e.Response.Request.Method,
+		e.Response.Request.URL,
+		e.Response.StatusCode,
+		e.Err)
+}
 
 // HTTP errors
 var (
